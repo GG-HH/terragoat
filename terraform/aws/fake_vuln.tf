4c8e9bd04853cc263601301c2f5e911993296c55
GG-HH <59922662+GG-HH@users.noreply.github.com>
2024-02-27 20:08:11 +0100

    Create fake_vuln.tf
    Add new vulns

---
+++ terraform/aws/fake_vuln.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "bad_example" {
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    # ggignore-iac: GG_IAC_0003
+    cidr_blocks = [  # ggignore-iac: GG_IAC_0003
+    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
+  }
+ }
