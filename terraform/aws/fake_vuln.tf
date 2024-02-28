7d9ffc815c682b0d4a80a83b846aec4e0317b1f7
GG-HH <59922662+GG-HH@users.noreply.github.com>
2024-02-28 10:18:18 +0100

    Update fake_vuln.tf
    Re add vulns without gg-ignore

--- terraform/aws/fake_vuln.tf
+++ terraform/aws/fake_vuln.tf
@@ -1,21 +1,19 @@
 # CONTAINS
 #   2 GG_IAC_0003
 # GG_IAC_0003 HIGH twice
-# ggignore-iac: GG_IAC_0003
-# resource "aws_security_group" "bad_example" {
-#  ingress {
-#    from_port = 80
-#    to_port   = 80
-#    protocol  = "tcp"
-#    # ggignore-iac: GG_IAC_0003
-#    cidr_blocks = [  # ggignore-iac: GG_IAC_0003
-#    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
-#  }
-#  ingress {
-#    from_port = 22
-#    to_port   = 22
-#    protocol  = "tcp"
-#    cidr_blocks = [
-#    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
-#  }
-# }
+resource "aws_security_group" "bad_example" {
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"] # very broad network subnet trigger GG_IAC_0003
+  }
+ }
